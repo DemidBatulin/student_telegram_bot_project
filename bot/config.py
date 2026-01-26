2a4d52dda2390df1321fdfe21edb8bd73ecd0af4
DemidBatulin <expressdave988@gmail.com>
2026-01-26 20:57:11 +0200

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -9,3 +9,4 @@ ONEC_BASE_URL = os.getenv("ONEC_BASE_URL", "")  # например: http://local
 ONEC_USER = os.getenv("ONEC_USER", "")          # если требуется
 ONEC_PASSWORD = os.getenv("ONEC_PASSWORD", "")  # если требуется
 ONEC_TIMEOUT = int(os.getenv("ONEC_TIMEOUT", "10"))
+

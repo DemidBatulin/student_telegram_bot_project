6215fcd6e622dbf5a3cc0fb67256da7301745f10
DemidBatulin <expressdave988@gmail.com>
2026-01-26 20:56:51 +0200

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,11 @@
-TELEGRAM_BOT_TOKEN = "8452453833:AAHkhjJlzR640Ma_OUFudwnrOLGYVml8lT0"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
+
+ONEC_BASE_URL = os.getenv("ONEC_BASE_URL", "")  # например: http://localhost/BASE/hs/StudentInfo/
+ONEC_USER = os.getenv("ONEC_USER", "")          # если требуется
+ONEC_PASSWORD = os.getenv("ONEC_PASSWORD", "")  # если требуется
+ONEC_TIMEOUT = int(os.getenv("ONEC_TIMEOUT", "10"))
